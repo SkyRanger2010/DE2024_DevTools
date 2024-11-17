f9c165a21c0ecb069fed8042747e5bf0735cd3ca
Василий Сатанцев <vasily.satantsev@gmail.com>
2024-11-17 22:39:52 +0300

    script_4.sh fixed

--- yandex_contest/script_4.sh
+++ yandex_contest/script_4.sh
@@ -15,20 +15,39 @@ function port() {
 	echo $res	
 }
 
+function arg_k() {
+	res=$(echo $1 | grep -Po "^.*?(?=\=)")
+	echo $res
+}
 
+> output.txt
 for item in $(cat input.txt) ; do
+	item=$(echo $item | sed 's/google/yandex/g')
 	sh=$(scheme $item)
 	if [ $sh ] ; then
-		echo "Scheme: $sh"
+		echo "Scheme: $sh" >> output.txt
 		item=$(echo $item |sed -r 's|'"$sh"'||')
 	fi
 	hst=$(host $item)
-	echo "Host: $hst"
+	echo "Host: $hst" >> output.txt
 	item=$(echo $item | sed -r 's|'"$hst"'||')
 	port=$(port $item)
 	if [ $port ] ; then
-		echo "Port: $port"
+		echo "Port: $port" >> output.txt
 	fi
 	item=$(echo $item | grep -Po "(?<=[?]).*" )
-	echo $item
+	OIFS=$IFS
+	IFS="&"
+	is_args=""
+	for arg in $item ; do
+		key=${arg%=*}
+		value=${arg#*=}
+		if [[ -z "$is_args" ]] ; then
+			echo "Args:" >> output.txt
+			is_args=1
+		fi	
+		echo "  Key: $key; Value:  $value" >> output.txt
+	done
+	IFS=$OIFS
+	echo >> output.txt
 done
