fbcee964938740861f6076600ac565d62c93ee6d
Василий Сатанцев <vasily.satantsev@gmail.com>
2024-11-17 16:16:20 +0300

    script_3.sh fixed

--- yandex_contest/script_3.sh
+++ yandex_contest/script_3.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 COUNT=1
-SUM=0
+SUM=$((0))
 for num in $(cat input.txt) ; do
 	if [ $COUNT -eq 1 ] ; then
 		X=$num
 		COUNT=$(( COUNT + 1 ))
 	else
-		POWER=$(( COUNT - 2)) 
-		SUM=$(( SUM + $num * $X^$POWER ))
+		POWER=$(( COUNT - 2 ))
+		STEP_SUM=$(( X**POWER ))
+		SUM=$(( SUM+num*STEP_SUM ))
 		COUNT=$(( COUNT + 1 ))
 	fi
 done
-echo $(( $SUM%(10^9+7) )) > output.txt
+RES=$((SUM%(10^9+7)))
+echo $RES  > output.txt
