d84faebe093665afb5fb4bbcfdbe059ef09f1f6a
SkyRanger2010 <30374628+SkyRanger2010@users.noreply.github.com>
2024-11-17 13:48:40 +0300

    Update script_1.sh

--- yandex_contest/script_1.sh
+++ yandex_contest/script_1.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+LINES=$(wc -l input.txt | awk '{print $1}')
+WORDS=$(wc -w input.txt | awk '{print $1}')
+LETTERS=$(grep -o '[A-Za-z]' input.txt | wc -l)
+
+echo "Input file contains:
+$LETTERS letters
+$WORDS words
+$LINES lines" > output.txt
