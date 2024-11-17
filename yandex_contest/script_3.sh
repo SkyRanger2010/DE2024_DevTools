01befa18bcb6d00e38e948f3a715c65cfa4f9e63
Василий Сатанцев <vasily.satantsev@gmail.com>
2024-11-17 18:22:14 +0300

    script_3.sh fixed

--- yandex_contest/script_3.sh
+++ yandex_contest/script_3.sh
@@ -2,15 +2,21 @@
 cnt=1
 sum=$((0))
 mod=$((10**9+7))
+prev=$((0))
 for num in $(cat input.txt) ; do
 	if [ $cnt -eq 1 ] ; then
 		x=$num
+		prev=$((x%mod))
 		cnt=$(( cnt + 1 ))
 	else
-		
-		power=$(( cnt - 2 ))
-		sum=$(awk "BEGIN {print ($sum+$num*$x^$power)%$mod}")
-		cnt=$(( cnt + 1 ))
+		if [ $cnt -eq 2 ] ; then
+			sum=$((num%mod))
+			cnt=$(( cnt +1 ))
+		else
+			prev=$((prev*x%mod))
+			sum=$(( (sum+num*prev)%mod ))
+			cnt=$(( cnt + 1 ))
+		fi
 	fi
 done
 echo $sum  > output.txt
